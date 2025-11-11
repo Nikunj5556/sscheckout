df294c7a8936103e339bf656bfd4a813ee2387a4
Nikunj5556 <nikunja412@gmail.com>
2025-11-11 15:39:44 +0530

    Update OAuth install URL to use dynamic scopes

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,11 +1,14 @@
 export default async function handler(req, res) {
   const shop = req.query.shop;
+
   if (!shop) {
     return res.status(400).send("Missing ?shop parameter");
   }
 
   const redirectUri = `${process.env.BASE_URL}/api/auth-callback`;
-  const installUrl = `https://${shop}/admin/oauth/authorize?client_id=${process.env.SHOPIFY_API_KEY}&scope=${process.env.SCOPES}&redirect_uri=${redirectUri}`;
-
+  const scopes = process.env.SCOPES || 'read_products,write_orders,read_discounts';
+  
+  const installUrl = `https://${shop}/admin/oauth/authorize?client_id=${process.env.SHOPIFY_API_KEY}&scope=${scopes}&redirect_uri=${redirectUri}`;
+  
   return res.redirect(installUrl);
 }
